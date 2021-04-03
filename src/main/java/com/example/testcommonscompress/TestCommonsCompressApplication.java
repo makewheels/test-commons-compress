eb249d7232bf9ca2af4ad6d3eb971833a174423c
makewheels <makewheels>
2021-04-03 10:25:34 +0800

    Initial commit

---
+++ src/main/java/com/example/testcommonscompress/TestCommonsCompressApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcommonscompress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCommonsCompressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCommonsCompressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testcommonscompress/TestCommonsCompressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcommonscompress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCommonsCompressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
