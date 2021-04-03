4f943504ef922c86fb714eeefae3821a196e8f5e
makewheels <makewheels>
2021-04-03 11:32:22 +0800

    folderPath

--- src/main/java/com/example/testcommonscompress/Test.java
+++ src/main/java/com/example/testcommonscompress/Test.java
@@ -6,13 +6,14 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        File f1 = new File("C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\pom.xml1");
-        File f2 = new File("C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\pom.xml2");
-        File f3 = new File("C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\pom.xml3");
-        File f4 = new File("C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\pom.xml4");
+        String folderPath = "C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\";
+        File f1 = new File(folderPath + "pom.xml1");
+        File f2 = new File(folderPath + "pom.xml2");
+        File f3 = new File(folderPath + "pom.xml3");
+        File f4 = new File(folderPath + "pom.xml4");
 
         List<File> files = Arrays.asList(f1, f2, f3, f4);
-        File zipFile = new File("C:\\Users\\thedoflin\\Downloads\\testcommonscompress\\t.zip");
+        File zipFile = new File(folderPath + "t.zip");
 
 //        ZipUtil.compress(files, zipFile, true);
         System.out.println(ZipUtil.decompress(zipFile, zipFile.getParentFile()));
